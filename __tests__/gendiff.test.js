91fcca8f0509d30b8ce7068df138dad28805e1ab
Pavel <pa.kachalov@gmail.com>
2017-07-25 16:12:37 +0300

    change options

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,5 @@
 import gendiff from '../src/gendiff';
 
-test('adds 2 + 3 to equal 5', () => {  
+test('adds 2 + 3 to equal 5', () => {
   expect(gendiff(2, 3)).toBe(5);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import program from 'commander';
 import gendiff from './gendiff';
+import setings from '../package.json';
 
 export default () => program
+        .version(setings.version)
         .arguments('<firstConfig> <secondConfig>')
-        .description('Compares two configuration files and shows a difference.')
-        .option('-V, --version', 'output the version number')
+        .description(setings.description)
         .option('-f, --format [type]', 'Output format')
         .action((first, second) => gendiff(first, second))
         .parse(process.argv);
