fb8b795f19a09e9323f9d821d068201cf857719a
Pavel <pa.kachalov@gmail.com>
2017-07-25 11:49:54 +0300

    add jest

--- Makefile
+++ Makefile
@@ -4,6 +4,12 @@ install:
 start:
 	npm run babel-node -- src/bin/gendiff.js
 
+build:
+	npm build
+
+test:
+	npm test
+
 publish:
 	npm publish
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import program from 'commander';
+import gendiff from './gendiff';
 
 export default () => program
         .arguments('<firstConfig> <secondConfig>')
         .description('Compares two configuration files and shows a difference.')
         .option('-V, --version', 'output the version number')
         .option('-f, --format [type]', 'Output format')
+        .action((first, second) => gendiff(first, second))
         .parse(process.argv);
