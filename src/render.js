4baffe3f8f2635fbf924a7d486b513efa213fba5
Pavel <pa.kachalov@gmail.com>
2017-07-27 17:29:49 +0300

    fix style

--- src/render.js
+++ src/render.js
@@ -1,3 +1,1 @@
-
-
-export default (data) => '\n{\n '.concat(`${data.map(item => item.toString()).join('\n ')}`).concat('\n}\n');
+export default data => '\n{\n '.concat(`${data.map(item => item.toString()).join('\n ')}`).concat('\n}\n');
