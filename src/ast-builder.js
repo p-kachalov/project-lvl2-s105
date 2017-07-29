f4567ae2f14aa01a89b0d5294e04230f6ba13056
Pavel <pa.kachalov@gmail.com>
2017-07-29 12:43:54 +0300

    refactoring ast builder

--- src/ast-builder.js
+++ src/ast-builder.js
@@ -3,18 +3,39 @@ import lodash from 'lodash';
 const buildAst = (firstObject, secondObject) => {
   const keys = lodash.union(Object.keys(firstObject), Object.keys(secondObject));
 
-  return keys.map((key) => {
-    const oldValue = firstObject[key];
-    const newValue = secondObject[key];
+  const isValuesObjects = key =>
+    lodash.isObject(firstObject[key]) && lodash.isObject(secondObject[key]);
 
-    if (oldValue === newValue) return { key, newValue, oldValue, type: 'nochanged' };
-    if (oldValue && newValue) {
-      return (lodash.isObject(oldValue) && lodash.isObject(newValue)) ?
-        { key, newValue, oldValue: buildAst(oldValue, newValue), type: 'nochanged' } :
-        { key, newValue, oldValue, type: 'changed' };
-    }
-    if (oldValue) return { key, newValue, oldValue, type: 'deleted' };
-    return { key, newValue, oldValue, type: 'added' };
+  const isValuesEqual = key =>
+    firstObject[key] === secondObject[key] || isValuesObjects(key);
+
+  const status = {
+    wasRemoved: key =>
+      lodash.has(firstObject, key) && !lodash.has(secondObject, key),
+
+    wasAdded: key =>
+      !lodash.has(firstObject, key) && lodash.has(secondObject, key),
+
+    wasChanged: key =>
+      lodash.has(firstObject, key) && lodash.has(secondObject, key) &&
+      !(isValuesEqual(key)),
+
+    wasUnchanged: key =>
+      (lodash.has(firstObject, key) && lodash.has(secondObject, key)) &&
+      isValuesEqual(key),
+  };
+
+  const makeNode = (key, type) => {
+    const newValue = secondObject[key];
+    const oldValue = isValuesObjects(key) ? buildAst(firstObject[key], newValue) : firstObject[key];
+    return { key, newValue, oldValue, type };
+  };
+
+  return keys.map((key) => {
+    if (status.wasRemoved(key)) return makeNode(key, 'removed');
+    if (status.wasAdded(key)) return makeNode(key, 'added');
+    if (status.wasChanged(key)) return makeNode(key, 'changed');
+    return makeNode(key, 'unchanged');
   });
 };
 
--- src/render.js
+++ src/render.js
@@ -4,10 +4,10 @@ const renderData = (data) => {
   const renderValue = value => (lodash.isArray(value) ? renderData(value) : value);
 
   const template = {
-    nochanged: item => `  ${item.key}: ${renderValue(item.oldValue)}`,
+    unchanged: item => `  ${item.key}: ${renderValue(item.oldValue)}`,
     changed: item => `+ ${item.key}: ${renderValue(item.newValue)}`.concat('\n ').concat(`- ${item.key}: ${renderValue(item.oldValue)}`),
     added: item => `+ ${item.key}: ${renderValue(item.newValue)}`,
-    deleted: item => `- ${item.key}: ${renderValue(item.oldValue)}`,
+    removed: item => `- ${item.key}: ${renderValue(item.oldValue)}`,
   };
 
   const dataString = data.map(item => template[item.type](item)).join('\n ');
