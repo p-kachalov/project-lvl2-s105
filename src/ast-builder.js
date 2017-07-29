00f4fe4721244fc19756a2b33532b20cc056a3f8
Pavel <pa.kachalov@gmail.com>
2017-07-29 22:13:56 +0300

    nested config

--- src/ast-builder.js
+++ src/ast-builder.js
@@ -1,15 +1,12 @@
 import lodash from 'lodash';
 
-const buildAst = (first, second) => {
-  const firstObject = first || {};
-  const secondObject = second || {};
+const buildAst = (firstObject, secondObject) => {
   const keys = lodash.union(Object.keys(firstObject), Object.keys(secondObject));
-
   const isValuesObjects = key =>
     lodash.isObject(firstObject[key]) || lodash.isObject(secondObject[key]);
 
   const isValuesEqual = key =>
-    firstObject[key] === secondObject[key] || isValuesObjects(key);
+    firstObject[key] === secondObject[key];
 
   const status = {
     wasRemoved: key =>
@@ -20,19 +17,31 @@ const buildAst = (first, second) => {
 
     wasChanged: key =>
       lodash.has(firstObject, key) && lodash.has(secondObject, key) &&
-      !(isValuesEqual(key)),
+      !(isValuesEqual(key) || isValuesObjects(key)),
 
     wasUnchanged: key =>
       (lodash.has(firstObject, key) && lodash.has(secondObject, key)) &&
-      isValuesEqual(key),
+      (isValuesEqual(key) && !(isValuesObjects(key))),
   };
 
   const makeNode = (key, type) => {
-    const newValue = isValuesObjects(key) ?
-      buildAst(firstObject[key], secondObject[key]) : secondObject[key];
-    const oldValue = isValuesObjects(key) ?
-      buildAst(firstObject[key], secondObject[key]) : firstObject[key];
-
+    if (type === 'group') {
+      const oldValue = buildAst(firstObject[key], secondObject[key]);
+      const newValue = buildAst(firstObject[key], secondObject[key]);
+      return { key, newValue, oldValue, type };
+    }
+    if (type === 'removed') {
+      const oldValue = lodash.isObject(firstObject[key]) ?
+        buildAst(firstObject[key], firstObject[key]) : firstObject[key];
+      return { key, newValue: oldValue, oldValue, type };
+    }
+    if (type === 'added') {
+      const newValue = lodash.isObject(secondObject[key]) ?
+        buildAst(secondObject[key], secondObject[key]) : secondObject[key];
+      return { key, newValue, oldValue: newValue, type };
+    }
+    const newValue = secondObject[key];
+    const oldValue = firstObject[key];
     return { key, newValue, oldValue, type };
   };
 
@@ -40,7 +49,8 @@ const buildAst = (first, second) => {
     if (status.wasRemoved(key)) return makeNode(key, 'removed');
     if (status.wasAdded(key)) return makeNode(key, 'added');
     if (status.wasChanged(key)) return makeNode(key, 'changed');
-    return makeNode(key, 'unchanged');
+    if (status.wasUnchanged(key)) return makeNode(key, 'unchanged');
+    return makeNode(key, 'group');
   });
 };
 
