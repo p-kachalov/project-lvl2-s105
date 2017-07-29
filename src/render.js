1759c99770ac5d0c2011182f235b07dfd34648ba
Pavel <pa.kachalov@gmail.com>
2017-07-29 14:23:49 +0300

    refactoring render

--- src/render.js
+++ src/render.js
@@ -1,13 +1,19 @@
 import lodash from 'lodash';
 
+const flag = {
+  added: '+',
+  removed: '-',
+  unchanged: ' ',
+};
+
 const renderData = (data) => {
   const renderValue = value => (lodash.isArray(value) ? renderData(value) : value);
 
   const template = {
-    unchanged: item => `  ${item.key}: ${renderValue(item.oldValue)}`,
-    changed: item => `+ ${item.key}: ${renderValue(item.newValue)}`.concat('\n ').concat(`- ${item.key}: ${renderValue(item.oldValue)}`),
-    added: item => `+ ${item.key}: ${renderValue(item.newValue)}`,
-    removed: item => `- ${item.key}: ${renderValue(item.oldValue)}`,
+    unchanged: item => `${flag.unchanged} ${item.key}: ${renderValue(item.oldValue)}`,
+    changed: item => `${flag.added} ${item.key}: ${renderValue(item.newValue)}`.concat('\n ').concat(`${flag.removed} ${item.key}: ${renderValue(item.oldValue)}`),
+    added: item => `${flag.added} ${item.key}: ${renderValue(item.newValue)}`,
+    removed: item => `${flag.removed} ${item.key}: ${renderValue(item.oldValue)}`,
   };
 
   const dataString = data.map(item => template[item.type](item)).join('\n ');
