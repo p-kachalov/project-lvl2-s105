41228fcc2014e34d4e519b1d4aec9f3e0ecde9fa
Pavel <pa.kachalov@gmail.com>
2017-07-30 17:18:53 +0300

    remove reader

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import reader from './reader';
+import fs from 'fs';
+import path from 'path';
 import parser from './parser';
 import buildDiffAst from './ast-builder';
 import render from './renders';
 
 export default (firstFilePath, secondFilePath, format) => {
-  const firstFile = reader(firstFilePath);
-  const secondFile = reader(secondFilePath);
+  const firstFileData = fs.readFileSync(firstFilePath, 'utf-8');
+  const firstFileType = path.extname(firstFilePath).split('.')[1];
 
-  const firstObject = parser(firstFile.data, firstFile.type);
-  const secondObject = parser(secondFile.data, secondFile.type);
+  const secondFileData = fs.readFileSync(secondFilePath, 'utf-8');
+  const secondFileType = path.extname(secondFilePath).split('.')[1];
+
+  const firstObject = parser(firstFileData, firstFileType);
+  const secondObject = parser(secondFileData, secondFileType);
 
   const diffAst = buildDiffAst(firstObject, secondObject);
   return render(diffAst, format);
--- src/reader.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const readData = dataPath => fs.readFileSync(dataPath, 'utf-8');
-
-const getType = dataPath => path.extname(dataPath).split('.')[1];
-
-export default filePath => ({ data: readData(filePath), type: getType(filePath) });
