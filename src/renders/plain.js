be7e697148eb379bc2b7da0ca115f1ef4c6b9fea
Pavel <pa.kachalov@gmail.com>
2017-07-30 20:37:07 +0300

    renaming

--- src/renders/plain.js
+++ src/renders/plain.js
@@ -4,12 +4,12 @@ const renderPlainValue = value => (lodash.isObject(value) ? 'complex value' : va
 
 const plainRender = (data, path) =>
   data.map((item) => {
-    const name = path.concat(item.key);
+    const fullName = path.concat(item.key);
     if (item.type === 'unchanged') return '';
-    if (item.type === 'nested') return plainRender(item.children, name);
-    if (item.type === 'added') return `Property '${name.join('.')}' was added with value: ${renderPlainValue(item.newValue)}\n`;
-    if (item.type === 'removed') return `Property '${name.join('.')}' was removed\n`;
-    if (item.type === 'changed') return `Property '${name.join('.')}' was updated. From '${item.oldValue}' to '${item.newValue}'\n`;
+    if (item.type === 'nested') return plainRender(item.children, fullName);
+    if (item.type === 'added') return `Property '${fullName.join('.')}' was added with value: ${renderPlainValue(item.newValue)}\n`;
+    if (item.type === 'removed') return `Property '${fullName.join('.')}' was removed\n`;
+    if (item.type === 'changed') return `Property '${fullName.join('.')}' was updated. From '${item.oldValue}' to '${item.newValue}'\n`;
     return '';
   }).join('');
 
